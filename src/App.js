a239f3976c81a415bed47eebbafff361a7e28dab
fliderman01 <fliderman01@gmail.com>
2022-01-08 13:39:10 +0400

    1) usesd useEffect for localStorage, 2) for addPanel added 'panel.length + 1'

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Panel from './components/Panel';
 import Adder from './components/Adder';
 import './style.css';
 
 export default function App() {
-  const [panel, setPanel] = useState([]);
+  const [panel, setPanel] = useState(() => {
+    // put 'panel' value from localStorage to panel
+    const saved = localStorage.getItem('panel');
+    const initialValue = JSON.parse(saved);
+    return initialValue || [];
+  });
   // add new panel
   const addNew = () => {
     setPanel([
       ...panel,
       {
-        id: panel.length,
+        id: panel.length + 1,
       },
     ]);
-    const newAray = panel;
-    localStorage.setItem('Panel', JSON.stringify(newAray));
   };
+  // Save/update panel value in local Storage (on render)
+  useEffect(() => {
+    localStorage.setItem('panel', JSON.stringify(panel));
+  });
   // delete individual panel
   const delPanel = (id) => {
     const remainingPanels = panel.filter((item) => id !== item.id);
     setPanel(remainingPanels);
-    localStorage.setItem('Panel', JSON.stringify(remainingPanels));
   };
 
   // delete All Panels
@@ -32,15 +38,14 @@ export default function App() {
     <div>
       <Panel />
       <Adder addNew={addNew} delAll={delAll} />
-      {localStorage.getItem('Panel') &&
-        JSON.parse(localStorage.getItem('Panel')).map((item) => (
-          <Panel
-            key={Math.floor(Math.random() * 9999)}
-            id={item.id}
-            gameNum={item.id + 2}
-            delPanel={delPanel}
-          />
-        ))}
+      {panel.map((item) => (
+        <Panel
+          key={Math.floor(Math.random() * 9999)}
+          id={item.id}
+          gameNum={item.id + 1}
+          delPanel={delPanel}
+        />
+      ))}
     </div>
   );
 }
